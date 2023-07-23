cad651a387e7b913db4311ea1bd681c4d14a3998
Supreet <singh.supreet14@gmail.com>
2023-07-23 11:52:34 +0530

    Updated to ratatui, notes made dynamic from config.

--- src/backend/messages.rs
+++ src/backend/messages.rs
@@ -1,4 +1,5 @@
 use crate::backend::config::Config; 
+use chrono::datetime;
 use serde::{Deserialize, Serialize};
 use tui::style::Color; 
 
@@ -21,6 +22,7 @@ pub struct Message {
     color: Color,
     x: u16,
     y: u16,
+    date: datetime,
 }
 
 
@@ -38,33 +40,17 @@ impl Message {
     }
 
      pub fn get_x(&self) -> u16 {
-         self.x.clone()
+         self.x
      }
 
      pub fn get_y(&self) -> u16 {
-         self.y.clone()
-     }
-
-     pub fn get_width(&self) -> u16 {
-         let mut length = self.get_message_text().len();
-         if length > 15 {
-             length = 15; 
-         } 
-         let r :u16 = 25;
-         r
-     }
-
-     pub fn get_height(&self) -> u16 {
-         let length = self.get_message_text().len(); 
-         // (length as u16) / 15
-         let r :u16 = 10;
-         r
+         self.y
      }
 
 }
 
 pub fn parse_config(config: Config) -> Result<Messages, String> {
-    serde_json::from_str(&config.get_config_text().as_str()).
+    serde_json::from_str(config.get_config_text().as_str()).
         map_err(|e| e.to_string())
 }
 
--- src/ui/notes.rs
+++ src/ui/notes.rs
@@ -1,3 +1,4 @@
+use tui::prelude::{Direction, Alignment};
 use tui::style::{Color, Style};
 use tui::backend::CrosstermBackend;
 use tui::layout::{Layout, Constraint, Rect};
@@ -6,45 +7,85 @@ use tui::{Terminal, layout};
 use std::io::{stdout, Stdout}; 
 use crate::backend::messages::{Messages, Message}; 
 
-
-
-pub fn generate_note_template() -> Result<Terminal<CrosstermBackend<Stdout>>, String>  {
-    let mut term = match Terminal::new(CrosstermBackend::new(std::io::stdout())) {
-        Ok(t) => t, 
-        Err(e) => return Err(e.to_string()),
-    };
-
-    term.draw(|f| {
-        let size = f.size(); 
-        let chunks = Layout::default()
-            .constraints([
-                         Constraint::Length(3),
-                         Constraint::Length(3),
-                         Constraint::Min(3),
-            ].as_ref()
-            ).split(size);
-
-    }).unwrap(); 
-
-    Ok(term)
+// Create a basic Rect which has the specified layout and it will split it.
+// the size of the rectangle has to depend on the messages.
+fn create_rect_for_notes(msg: &Message) -> Rect {
+    let r: Rect = Rect::new(msg.get_x(), msg.get_y(),40,16); 
+    r
 }
 
-fn create_rect_for_notes(msg: Message) -> Rect {
-    let rect: Rect = Rect::new(msg.get_x(), msg.get_y(), msg.get_width(), msg.get_height()); 
-    rect
-}
 
+    //term.draw(|f| {
+    //    // Make a unique layout for every note.
+    //    let r = create_rect_for_notes(msg); 
+    //    let chunks = Layout::default().direction(Direction::Horizontal)
+    //        // the constraints have to set dynamically
+    //        .constraints([
+    //              Constraint::Length(25),
+    //              Constraint::Length(25),
+    //              Constraint::Length(25),
+    //              Constraint::Length(25),
+
+    //        ].as_ref()).split(r); 
+
+    //    for c in chunks.into_iter() {
+    //        let license = Paragraph::new() 
+    //            .alignment(layout::Alignment::Center)
+    //            .style(Style::default().bg(Color::Green).fg(Color::Red)); 
+
+    //        f.render_widget(license, *c); 
+    //    }
+
+    //}).unwrap(); 
+
+
+// how about we make rectangle for a single note, then add constraints to it, render it and keep
 pub fn render_notes(msg: &Messages) {
     let mut term = Terminal::new(CrosstermBackend::new(std::io::stdout())).unwrap(); 
+    
+    term.draw(|f| {
 
-    for m in msg.messages.clone() {
-        term.draw(|f| {
-                let license = Paragraph::new(m.get_message_text())
-                    .alignment(layout::Alignment::Center)
-                    .style(Style::default().bg(Color::Green).fg(Color::Red)); 
+        for m in msg.messages.clone().into_iter() {
+            let rect = create_rect_for_notes(&m);
+            let chunks = Layout::default().
+                direction(Direction::Vertical).
+                constraints([
+                            Constraint::Ratio(1, 4),
+                            Constraint::Ratio(2, 4),
+                            Constraint::Ratio(1, 4),
+                ].
+                as_ref()).
+                split(rect);
 
-            f.render_widget(license, create_rect_for_notes(m.clone()));
-        }).unwrap(); 
-    }
+            for (index, r) in chunks.into_iter().enumerate() {
+                match index {
+                    0 => {
+                        let title = Paragraph::new(m.get_message_id().to_string()).
+                            style(Style::default().bg(*m.get_note_color())).
+                            alignment(Alignment::Center);
+
+                        f.render_widget(title, *r); 
+                    }, 
+                    1 => {
+                        let mess = Paragraph::new(m.get_message_text().clone()).
+                            style(Style::default().bg(*m.get_note_color())).
+                            alignment(Alignment::Left);
+
+                        f.render_widget(mess, *r); 
+                    } 2 => {
+                        let mess = Paragraph::new(m.get_message_text().clone()).
+                            style(Style::default().bg(*m.get_note_color())).
+                            alignment(Alignment::Left);
+
+                        f.render_widget(mess, *r); 
+                    }
+                    _ => {
+                        println!("Missed it"); 
+                    }
+                }
+            }
+
+        }
+    }).unwrap();
 }
 
