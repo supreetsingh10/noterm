cc1a215fbbcbaa3164c6246c10faf1a1a03d8ba6
Supreet <singh.supreet14@gmail.com>
2023-08-06 13:33:13 +0530

    fixed the coordinates and sizing of the notes.

--- src/backend/constants.rs
+++ src/backend/constants.rs
@@ -1,7 +1,7 @@
 pub const CONFIG_PATH: &str = "/.config/sticky_notes/config.json"; 
 pub const ENV_HOME_VAR: &str = "HOME"; 
 
-pub const WIDTH: u16 = 40;
+pub const NOTE_WIDTH: u16 = 40;
 
 #[derive(PartialEq, Debug)]
 pub enum SubCommandNames {
--- src/ui/notes.rs
+++ src/ui/notes.rs
@@ -1,5 +1,4 @@
 use std::io::{stdout, Stdout};
-
 use tui::prelude::{Direction, Alignment};
 use tui::style::{Color, Style};
 use tui::backend::CrosstermBackend;
@@ -8,7 +7,7 @@ use tui::widgets::{Paragraph, Wrap};
 use tui::{Terminal, layout};
 use tui::prelude::Text; 
 use crate::backend::messages::{Messages, Message}; 
-use crate::backend::constants::WIDTH; 
+use crate::backend::constants::NOTE_WIDTH; 
 
 
 // so we need a function that will handle the coordinate part of the function
@@ -18,12 +17,15 @@ use crate::backend::constants::WIDTH;
 // the width of the vector is fixed. 
 
 // next line will be because of /n, or if it exceeds the width 
-type coords = (u16, u16); 
+const INIT_X:u16 = 0; 
+const INIT_Y:u16 = 0; 
+//              x,y,width,height
+type Coords = (u16, u16, u16, u16); 
 fn count_note_lines(msg_text: &str) -> u16 {
     let mut height: u16 = 0; 
     let mut sen_len: u32 = 0;  
     for c in msg_text.chars().into_iter() {
-        if c == '\n' || sen_len >= WIDTH.into() {
+        if c == '\n' || sen_len >= NOTE_WIDTH.into() {
             sen_len = 0; 
             height += 1; 
         } else {
@@ -34,42 +36,102 @@ fn count_note_lines(msg_text: &str) -> u16 {
     height
 }
 
-
-// [(0, 3), (2, 0)]
-pub fn render(msg: &Messages) {
-    let mut term = Terminal::new(CrosstermBackend::new(stdout())).unwrap(); 
-    let coord_vec = fix_coordinates_of_notes(&msg, &term).unwrap(); 
-    // use these coordinates to render later.
-    //
-    term.draw(|f| {
-        let rect = f.size(); 
-    }); 
-
+fn rects_for_notes(coord: Coords) -> Rect {
+    Rect::new(coord.0, coord.1, coord.2, coord.3)
 }
 
-// Needs to be tested.
-fn fix_coordinates_of_notes(msg: &Messages, term: &Terminal<CrosstermBackend<Stdout>>) -> Result<Vec<coords>, String> {
+pub fn render(msg: &Messages) {
+    let mut term = Terminal::new(CrosstermBackend::new(stdout())).unwrap(); 
+    let coord_vec = fix_coordinates_of_notes(&msg, &mut term).unwrap(); 
+
+    term.draw(|f| {
+        for (m_it, c_it) in msg.messages.clone().iter().zip(coord_vec.iter()) {
+            let rect = rects_for_notes(c_it.to_owned()); 
+            let chunks = Layout::default().
+                direction(Direction::Vertical).
+                constraints([
+                            Constraint::Ratio(1,10),
+                            Constraint::Ratio(5, 2),
+                ].as_ref()).
+                split(rect);
+
+            for (index, r) in chunks.into_iter().enumerate() {
+                match index {
+                    0 => {
+                        let title = Paragraph::new(m_it.get_message_id().to_string()).
+                            style(Style::default().bg(*m_it.get_note_color())).
+                            alignment(Alignment::Center);
+
+                        f.render_widget(title,*r); 
+                    },
+                    1 => {
+                        let body = Paragraph::new(m_it.get_message_text().to_string()).
+                            style(Style::default().bg(*m_it.get_note_color())).
+                            alignment(Alignment::Left).
+                            wrap(Wrap {trim: true});
+
+                        f.render_widget(body,*r); 
+                    },
+                    _ => {
+                        println!("Missed it"); 
+                    }
+                }
+            }
+        }
+    }).unwrap(); 
+}
+
+/// Needs to be tested.
+// Make sure that it does not go out of the window
+// Check if we can get more space in the terminal if try to render something longer than 60 pixels.
+fn fix_coordinates_of_notes(msg: &Messages, term: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<Vec<Coords>, String> {
     let term_rect = match term.size() {
         Ok(s) => s,
         Err(e) => return Err(e.to_string()),
     }; 
 
-    let mut coords_vec: Vec<coords> = Vec::new(); 
-    let max_row_notes = term_rect.width / WIDTH; 
+    let mut coords_vec: Vec<Coords> = Vec::new(); 
+    let max_notes_in_row = term_rect.width / NOTE_WIDTH; 
     let mut row_of_note: u16; 
+    let mut row_change: bool = false; 
+    let mut last_row: u16 = 0; 
     for (index, m) in msg.messages.clone().into_iter().enumerate() {
-        let mut coord: coords = (0,0); 
+        let mut coord: Coords = (0,0,0,0); 
         let height;
-        row_of_note = (index as u16) / max_row_notes; 
-        if row_of_note > 0 {
-            height = (count_note_lines(&m.get_message_text())) + coords_vec[index - max_row_notes as usize].1; 
+        row_of_note = (index as u16) / max_notes_in_row; 
+
+        if row_of_note == last_row {
+            row_change = false; 
         } else {
-            height = count_note_lines(&m.get_message_text());
+            last_row = row_of_note;
+            row_change = true; 
         }
 
-        let width = WIDTH * index as u16;
-        coord.0 = width; 
-        coord.1 = height;
+        // height will be a variable for every note
+        height = count_note_lines(&m.get_message_text());
+
+        let width = NOTE_WIDTH; 
+        coord.2 = width; 
+        coord.3 = height + 10;
+
+
+        if index == 0 {
+            coord.0 = INIT_X;
+            coord.1 = INIT_Y;
+        } else {
+            if row_change {
+                // x,y,width,height
+                // set the new x and y of the note as the last x of the note above and y to be the
+                // y of the note above + height of the last note. 
+                coord.0 = coords_vec[index - max_notes_in_row as usize].0; 
+                coord.1 = coords_vec[index - max_notes_in_row as usize].1 + coords_vec[index - max_notes_in_row as usize].3; 
+            } else {
+                // x will be the x of last note + it's width.
+                // y will be the y of the last note. 
+                coord.0 = coords_vec[index - 1].0 + coords_vec[index - 1].2; 
+                coord.1 = coords_vec[index - 1].1; 
+            }
+        }
         coords_vec.push(coord); 
     }
 
